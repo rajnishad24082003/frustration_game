97c5055606650f44394b3ee4fece070bdcff61f8
[raj nishad] <[rajnishad24082003@gmail.com]>
2022-10-23 16:33:13 +0530

    some changes

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 let airplanes = document.getElementsByClassName("iconimg");
-
-airplanes[0].addEventListener("hover",()=>{
-    airplanes[0].style.color = "red";
-})
-
 for(let i=0;i<airplanes.length;i++)
 {
     let leftpositionstart = 10*i;
@@ -19,6 +14,14 @@ for(let i=0;i<airplanes.length;i++)
     airplanes[i].style.setProperty("--end-position-left",`${leftpositionend}%`);
     airplanes[i].style.setProperty("--end-position-top",`${toppositionend}%`);
 };
+for(let i=0;i<airplanes.length;i++)
+{
+    airplanes[i].style.setProperty("--moving-bottom-start",`0%`);
+    airplanes[i].style.setProperty("--moving-right-start",`0%`);
+    airplanes[i].style.setProperty("--moving-bottom-end",`10%`);
+    airplanes[i].style.setProperty("--moving-right-end",`10%`);
+};
+
 
 let airplanes2 = document.getElementsByClassName("iconimg2");
 for(let i=0;i<airplanes2.length;i++)
@@ -36,6 +39,13 @@ for(let i=0;i<airplanes2.length;i++)
     airplanes2[i].style.setProperty("--end-position-left",`${leftpositionend}%`);
     airplanes2[i].style.setProperty("--end-position-top",`${toppositionend}%`);
 };
+for(let i=0;i<airplanes2.length;i++)
+{
+    airplanes2[i].style.setProperty("--moving-bottom-start",`0%`);
+    airplanes2[i].style.setProperty("--moving-right-start",`0%`);
+    airplanes2[i].style.setProperty("--moving-bottom-end",`10%`);
+    airplanes2[i].style.setProperty("--moving-right-end",`10%`);
+};
 
 let airplanes3 = document.getElementsByClassName("iconimg3");
 for(let i=0;i<airplanes3.length;i++)
@@ -52,4 +62,11 @@ for(let i=0;i<airplanes3.length;i++)
     let toppositionend = 45;
     airplanes3[i].style.setProperty("--end-position-left",`${leftpositionend}%`);
     airplanes3[i].style.setProperty("--end-position-top",`${toppositionend}%`);
+};
+for(let i=0;i<airplanes3.length;i++)
+{
+    airplanes3[i].style.setProperty("--moving-bottom-start",`0%`);
+    airplanes3[i].style.setProperty("--moving-right-start",`0%`);
+    airplanes3[i].style.setProperty("--moving-bottom-end",`10%`);
+    airplanes3[i].style.setProperty("--moving-right-end",`10%`);
 };
